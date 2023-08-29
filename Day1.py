f82c6afdecba6459def5270b049e574531bb6af0
Gambit-01 <115642009+Gambit-01@users.noreply.github.com>
2023-08-29 23:01:47 +0200

    Delete Day1.py

--- Day1.py
+++
@@ -1,8 +0,0 @@
-#Greeting for program
-print("Welcome to the Band Name Generator")
-#Ask user for city they grew up in
-city = input("What's the name of the city you grew up in?\n")
-#Ask user for name of pet
-petname = input("What's the name of your pet?\n")
-#Combine the name of their city and pet and shwo their band name
-print("Your band name could be " + city + petname)
